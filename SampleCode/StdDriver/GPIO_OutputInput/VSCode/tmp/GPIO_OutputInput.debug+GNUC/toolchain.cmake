36bfe4675d259cdb11e549f1b45ba98f3172cbea
wright0418 <wright0418.wu@gmail.com>
2026-02-08 11:08:59 +0800

    gitignore: ignore build/out/tmp and remove tracked build files from index

--- SampleCode/StdDriver/GPIO_OutputInput/VSCode/tmp/GPIO_OutputInput.debug+GNUC/toolchain.cmake
+++
@@ -1,5 +0,0 @@
-# toolchain.cmake
-
-set(REGISTERED_TOOLCHAIN_ROOT "c:/Users/Wright/.vcpkg/artifacts/2139c4c6/compilers.arm.arm.none.eabi.gcc/14.3.1/bin")
-set(REGISTERED_TOOLCHAIN_VERSION "14.3.1")
-include("${CMSIS_COMPILER_ROOT}/GCC.10.3.1.cmake")
